0bf0c8e20c76371e5e3889e816ed6fd6e385946f
emmawojcik <71179439+emmawojcik@users.noreply.github.com>
2022-07-26 20:45:21 +0100

    image lights up when key or button pressed

--- app.js
+++ app.js
@@ -1,35 +1,57 @@
 const drumkit = document.getElementsByClassName('drum');
+const drumImage = document.getElementById('drumImage');
 
+// Given file path, play corresponding wav file
 function playSound(audioPath) {
   let drumSound = new Audio(audioPath);
   drumSound.play();
 }
 
+// Increase brightness of drums image
+function lightUpImage(){
+  drumImage.style.filter = 'brightness(130%)';
+  setTimeout(() => {
+    drumImage.style.filter = 'brightness(100%)';
+  }, 500);
+}
+
+// Turn drumkit into array and add event listener to each drum button
 Array.from(drumkit).forEach(drum => {
-  drum.addEventListener('click', (event) => {
+  drum.addEventListener('click', () => {
+    lightUpImage();
     playSound(`sounds/${drum.id}.wav`);
   });
 });
 
+// Add event listener for relevant keyboard presses
 document.addEventListener('keydown', (event) => {
   if(event.key == 'a'){
+    lightUpImage();
     playSound('sounds/boom.wav');
   }
   else if (event.key == 's'){
+    lightUpImage();
     playSound('sounds/clap.wav');
   } else if (event.key == 'd'){
+    lightUpImage();
     playSound('sounds/hihat.wav');
   } else if (event.key == 'f'){
+    lightUpImage();
     playSound('sounds/kick.wav');
   } else if (event.key == 'g'){
+    lightUpImage();
     playSound('sounds/openhat.wav');
   } else if (event.key == 'h'){
+    lightUpImage();
     playSound('sounds/ride.wav');
   } else if (event.key == 'j'){
+    lightUpImage();
     playSound('sounds/snare.wav');
   } else if (event.key == 'k'){
+    lightUpImage();
     playSound('sounds/tink.wav');
   } else if (event.key == 'l'){
+    lightUpImage();
     playSound('sounds/tom.wav');
   }
 });
